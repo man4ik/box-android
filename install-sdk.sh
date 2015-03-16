3189794f4fe49b4f3318e052e1991b7c5e9d9525
Eldar Mensutov <mensutove@gmail.com>
2015-03-16 16:07:32 +0200

    Update install-sdk.sh

--- install-sdk.sh
+++ install-sdk.sh
@@ -14,16 +14,13 @@ export PATH="${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools"
 
 type android || { echo "Path: $PATH"; echo 'Android not availble after installation, terminating.'; exit 1; }
 
-export ANDROID_BUILD_TOOLS="21.1.1"
+export ANDROID_BUILD_TOOLS="21.1.2"
 export ANDROID_VERSION="5.0"
 source $WERCKER_SOURCE_DIR/install-build-tools.sh
 
 # export ANDROID_BUILD_TOOLS="17"
 # export ANDROID_VERSION="4.2.2"
 # source $WERCKER_SOURCE_DIR/install-build-tools.sh
-
-export ANDROID_BUILD_TOOLS="21.1.1"
-
 
 # Write environment variables setup to $profile
 echo "export ANDROID_HOME=\"${ANDROID_HOME}\"" | sudo tee -a $profile
